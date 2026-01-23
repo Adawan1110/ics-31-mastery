1b5211dbcdf1cfef15643dfa75494a1dc830fdfe
Adawan1110 <adamsuh2005@gmail.com>
2026-01-23 09:11:45 -0800

    File updated at 1/23/2026, 9:11:43 AM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,24 +1,26 @@
-# Last updated: 1/19/2026, 2:32:36 PM
-1# class Solution(object):
-2#     def twoSum(nums, target):
-3#         for x in range(len(nums)):
-4#             for y in range(1, len(nums)):
-5#                 if nums[x] + nums[y] == target:
-6#                     z = (nums[x], nums[y])
-7#                     print(z)
-8#                 else:
-9#                     print("Error")
-10
-11class Solution(object):
-12    def twoSum(self, nums, target):
-13        val_idx = {} # key is val, val is index
+# Last updated: 1/23/2026, 9:11:43 AM
+1class Solution(object):
+2    def isPalindrome(self, x):
+3        """
+4        :type x: int
+5        :rtype: bool
+6        """
+7
+8        new_x = str(x)
+9
+10        x_list = []
+11
+12        for char in new_x:
+13            x_list.append(char)
 14
-15        for i, num in enumerate(nums):
-16            if target - num in val_idx:
-17                return (i, val_idx[target - num])
-18            val_idx[num] = i
-19
-20            
-21
-22
-23        
+15        y_list = x_list[:]
+16
+17        y_list.reverse()
+18
+19        if x_list == y_list:
+20            return True
+21        else:
+22            return False
+23
+24
+25
